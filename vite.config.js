8428bddcc0b9568d5fbd8ceef4a68af43e8fed5a
Vicente  Alvarez <47174830-petealvarez2005@users.noreply.replit.com>
2025-12-22 02:34:57 +0000

    Fix BrowserRouter for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/terapeuta-web/",
   plugins: [react()],
   server: {
     host: "0.0.0.0",
-    port: 5173,
     allowedHosts: [
       "d340b953-115b-4604-8f66-3945a1275777-00-2unguwyvoaq44.riker.replit.dev"
     ]
