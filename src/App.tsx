0d4771b7cdb27c635d3bd9eecb539e69b8d3d494
Vicente  Alvarez <47174830-petealvarez2005@users.noreply.replit.com>
2025-12-24 18:55:54 +0000

    deploy project

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ import Contact from "./pages/Contact";
 
 export default function App() {
   return (
-    <BrowserRouter basename="/terapeuta-web">
+    <BrowserRouter>
       <Navigation />
 
       <Routes>
