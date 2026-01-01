901f93d6f874bea8a6ab198f16416e25e9ac0adc
Vicente  Alvarez <47174830-petealvarez2005@users.noreply.replit.com>
2026-01-01 22:58:46 +0000

    Setup custom domain neurofamily.cl

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/terapeuta-web/" : "/",
+  base: "/",
   plugins: [react()],
   server: {
     host: "0.0.0.0",
